1f2fa8811dfeefddf1fc9297c0d7b497bad1b5f0
Hoàng Thịnh <shaynejohnson00@gmail.com>
2020-01-05 22:55:04 +0700

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin-login', { title: 'Express' });
+});
+
+router.get('/index.html', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/manage-admin.html', function(req, res, next) {
+  res.render('manage-admin', { title: 'Express' });
+});
+
+router.get('/edit-admin.html', function(req, res, next) {
+  res.render('edit-admin', { title: 'Express' });
+});
+
+router.get('/add-admin.html', function(req, res, next) {
+  res.render('add-admin', { title: 'Express' });
+});
+
+router.get('/admin-login.html', function(req, res, next) {
+  res.render('admin-login', { title: 'Express' });
+});
+
+
+module.exports = router;
